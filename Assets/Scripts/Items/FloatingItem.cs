bc166aec7c358d5f9c7f7f7a233b7de0ef687089
ddrosler <ddrosler@hotmail.com>
2025-08-01 15:24:57 -0400

    Added a new test scene to use in the main game scene. Floating items now only animate when the player is nearby and they have a bounce + spin offset

--- Assets/Scripts/Items/FloatingItem.cs
+++ Assets/Scripts/Items/FloatingItem.cs
@@ -1,17 +1,87 @@
 using UnityEngine;
 using DG.Tweening;
+
 public class FloatingItem : MonoBehaviour
 {
+    [Header("Animation Settings")]
     [SerializeField] float upwards_amount = 0.5f;
-    [SerializeField] float animation_duration_float = 2;
-    [SerializeField] float animation_duration_spin = 2;
+    [SerializeField] float animation_duration_float = 2f;
+    [SerializeField] float animation_duration_spin = 2f;
     [SerializeField] Ease ease_type = Ease.InOutQuad;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Performance Settings")]
+    [SerializeField] float triggerDistance = 100f;
+    [SerializeField] float checkDelay = 0.3f;
+    [SerializeField] float desyncDelayRange = 0.5f;
+
+    private bool isActive = false;
+    private bool wasActive = false;
+    private float delayTimer;
+
+    private Transform objectToAnimate;
+
+    private Tween floatTween;
+    private Tween spinTween;
+
     void Start()
     {
-        if(transform.childCount <= 0) return;
-        transform.GetChild(0).DOLocalMoveY(upwards_amount,animation_duration_float * 0.5f).SetLoops(-1,LoopType.Yoyo).SetEase(ease_type).SetLink(gameObject);
-        transform.GetChild(0).DOLocalRotate(new Vector3(0,360,0),animation_duration_spin,RotateMode.LocalAxisAdd).SetLoops(-1).SetEase(Ease.Linear).SetLink(gameObject);
+        delayTimer = Time.time + checkDelay;
+
+        objectToAnimate = transform;
+
+        float originalY = objectToAnimate.localPosition.y;
+        objectToAnimate.localPosition = new Vector3(objectToAnimate.localPosition.x, originalY + Random.Range(-0.1f, 0.1f), objectToAnimate.localPosition.z);
+
+        float randomDelay = Random.Range(0f, desyncDelayRange);
+
+        floatTween = objectToAnimate.DOLocalMoveY(originalY + upwards_amount, animation_duration_float * 0.5f)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(ease_type)
+            .SetLink(gameObject)
+            .SetDelay(randomDelay)
+            .Pause();
+
+        spinTween = objectToAnimate.DOLocalRotate(new Vector3(0, 360, 0), animation_duration_spin, RotateMode.LocalAxisAdd)
+            .SetLoops(-1)
+            .SetEase(Ease.Linear)
+            .SetLink(gameObject)
+            .SetDelay(randomDelay)
+            .Pause();
     }
 
+    void Update()
+    {
+        if (Time.time > delayTimer)
+        {
+            delayTimer = Time.time + checkDelay;
+
+            if (objectToAnimate == null || GameplayUtils.instance == null) return;
+
+            float dist = Vector3.Distance(GameplayUtils.instance.PlayerTransform.position, transform.position);
+            isActive = dist < triggerDistance;
+
+            if (isActive && !wasActive)
+            {
+                StartAnimating();
+            }
+            else if (!isActive && wasActive)
+            {
+                StopAnimating();
+            }
+
+            wasActive = isActive;
+        }
+    }
+
+    void StartAnimating()
+    {
+        floatTween.Play();
+        spinTween.Play();
+    }
+
+    void StopAnimating()
+    {
+        floatTween.Pause();
+        spinTween.Pause();
+    }
 }
