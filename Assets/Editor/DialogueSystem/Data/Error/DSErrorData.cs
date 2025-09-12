66c38ca0c71c5059337645ac4ee7995bfae994ee
ddrosler <ddrosler@hotmail.com>
2025-09-12 15:19:29 -0400

    Lots of crafting tutorial dialog work, typo fix on plank recipe, dialog errors are red, added an icon id for left click

--- Assets/Editor/DialogueSystem/Data/Error/DSErrorData.cs
+++ Assets/Editor/DialogueSystem/Data/Error/DSErrorData.cs
@@ -13,12 +13,13 @@ namespace DS.Data.Error
 
         private void GenerateRandomColor()
         {
-            Color = new Color32(
+            Color = Color.red;
+            /*Color = new Color32(
                 (byte) Random.Range(65, 256),
                 (byte) Random.Range(50, 176),
                 (byte) Random.Range(50, 176),
                 255
-            );
+            );*/
         }
     }
 }
