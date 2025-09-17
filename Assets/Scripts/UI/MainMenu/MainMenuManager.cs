37cf9e9a8f646807495e7981cac9f2c8c7ef5b4a
Arrow <arrowthefighter@gmail.com>
2025-09-17 12:27:48 +0200

    Main menu work

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -6,11 +6,25 @@ public class MainMenuManager : MonoBehaviour
     public GameObject MainMenuCam;
     public GameObject MainMenuCanvas;
     public GameObject MainMenuContents;
+
+    [Header("Main Panel")]
+    public GameObject MainPanel;
+    public GameObject PlayButton;
+
+    [Header("Settings Panel")]
+    public GameObject SettingsPanel;
+    public GameObject settingsBackButton;
+
+    [Header("Credits Panel")]
+    public GameObject CreditsPanel;
+    public GameObject creditsBackButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //GameplayUtils.instance.OpenMenu();
         StartCoroutine(freezePlayer());
+        OpenMainPanel();
     }
 
     IEnumerator freezePlayer()
@@ -29,5 +43,39 @@ public class MainMenuManager : MonoBehaviour
     public void HideMainMenuCanvas()
     {
         MainMenuCanvas.SetActive(false);
+    }
+
+    public void CloseAllPanels()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+    }
+
+    public void OpenMainPanel()
+    {
+        CloseAllPanels();
+        MainPanel.SetActive(true);
+        UIInputHandler.instance.ClosedMenu();
+        UIInputHandler.instance.defaultButton = PlayButton;
+        UIInputHandler.instance.OpenedMenu();
+    }
+
+    public void OpenSettingsPanel()
+    {
+        CloseAllPanels();
+        SettingsPanel.SetActive(true);
+        UIInputHandler.instance.ClosedMenu();
+        UIInputHandler.instance.defaultButton = settingsBackButton;
+        UIInputHandler.instance.OpenedMenu();
+    }
+
+    public void OpenCreditsPanel()
+    {
+        CloseAllPanels();
+        CreditsPanel.SetActive(true);
+        UIInputHandler.instance.ClosedMenu();
+        UIInputHandler.instance.defaultButton = creditsBackButton;
+        UIInputHandler.instance.OpenedMenu();
     }
 }
