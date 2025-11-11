eb93707a751d19a8771e066059a1767bbe75143e
Arrow <arrowthefighter@gmail.com>
2025-11-11 10:34:25 +0100

    fixed tree trunks being tinted and added bridge break animation

---
+++ Assets/Scripts/Cutscenes/Animations/PlayAnimationClip.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayAnimationClip : MonoBehaviour
+{
+    Animator animator;
+    public string defaultAnimName;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayClip(string clip_name)
+    {
+        animator.Play(clip_name);
+    }
+
+    public void PlayDefaultClip()
+    {
+        PlayClip(defaultAnimName);
+    }
+}
