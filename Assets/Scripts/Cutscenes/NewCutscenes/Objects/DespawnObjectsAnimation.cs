715d7727169878c810fb93db4d32255d5c541d61
Arrow <arrowthefighter@gmail.com>
2026-01-19 12:46:43 +0100

    blacksmith repair function

---
+++ Assets/Scripts/Cutscenes/NewCutscenes/Objects/DespawnObjectsAnimation.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+using System.Collections;
+using Sirenix.OdinInspector;
+
+public class DespawnObjectsAnimation : MonoBehaviour
+{
+    public List<GameObject> ObjectsToDespawn;
+
+    public float delay;
+    public float scaleOutDuration;
+    public Ease scaleOutEase;
+
+    public UnityEvent OnFinishedUnityEvent;
+
+    public AudioCollection[] scaleOutAudio;
+
+    [Button]
+    public void StartDespawnSequence()
+    {
+        StartCoroutine(DespawnSequenceCoroutine());
+    }
+
+    IEnumerator DespawnSequenceCoroutine()
+    {
+        for (int i = 0; i < ObjectsToDespawn.Count; i++)
+        {
+            ObjectsToDespawn[i].transform.DOScale(0,scaleOutDuration).SetEase(scaleOutEase);
+            //.OnComplete(() => {ObjectsToDespawn[i].SetActive(false);});
+            ScriptRefrenceSingleton.instance.soundFXManager.PlayAllSoundCollection(ObjectsToDespawn[i].transform, scaleOutAudio);
+            yield return new WaitForSeconds(delay);
+        }
+        OnFinishedUnityEvent?.Invoke();
+        gameObject.SetActive(false);
+    }
+}
