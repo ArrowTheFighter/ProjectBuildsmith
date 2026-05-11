aa4da80769e55aa6d18b8e5f89c44c34d6c736e8
Gideon Burnham <gideon@brimstone.games>
2026-05-11 20:41:30 +0200

    Fixed blacksmith not loading properly from a loaded saved

--- Assets/Scripts/Singletons/Save-Load/SaveEnabledState.cs
+++ Assets/Scripts/Singletons/Save-Load/SaveEnabledState.cs
@@ -4,12 +4,21 @@ public class SaveEnabledState : MonoBehaviour, ISaveable
 {
 
     public bool SaveWhenActive;
+    public bool OnlyCheckSelfActive;
 
     public int unique_id;
 
     public int Get_Unique_ID { get => unique_id; set{ unique_id = value; } }
 
-    public bool Get_Should_Save {get { return SaveWhenActive == gameObject.activeInHierarchy; }}
+    public bool Get_Should_Save {get 
+        {
+        if(OnlyCheckSelfActive)
+        {
+                return SaveWhenActive == gameObject.activeSelf;
+            }
+         return SaveWhenActive == gameObject.activeInHierarchy; 
+         }
+    }
 
     public void SaveLoaded(SaveFileStruct saveFileStruct)
     {
